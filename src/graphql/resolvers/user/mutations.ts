738cbf76c230587eb5f6016f0fce934409d0d11e
César Júnior <cesar.jnr@hotmail.com>
2022-01-19 19:27:39 -0300

    Add error handling to user mutations and queries

--- src/graphql/resolvers/user/mutations.ts
+++ src/graphql/resolvers/user/mutations.ts
@@ -1,5 +1,7 @@
 import { Container } from 'typedi';
+import { ApolloError } from 'apollo-server-express';
 
+import logger from '../../../utils/logger';
 import { UserCreationDTO, UserService } from '../../../services/UserService';
 import { User } from '../../../models/User';
 
@@ -7,12 +9,21 @@ const userMutations = {
   createUser: async (
     _: any,
     { user }: { user: UserCreationDTO }
-  ): Promise<Omit<User, 'password'>> => {
+  ): Promise<Omit<User, 'password'> | void> => {
+    try {
     const userService = Container.get<UserService>('userService');
     let newUser = await userService.create(user);
     newUser = Object.assign({}, newUser, { password: undefined });
 
     return newUser;
+    } catch (error: any) {
+      logger.error({
+        label: 'FindUser',
+        message: error.message
+      });
+
+      new ApolloError('Internal server error');
+    }
   }
 };
 
--- src/graphql/resolvers/user/queries.ts
+++ src/graphql/resolvers/user/queries.ts
@@ -1,25 +1,44 @@
 import { Container } from 'typedi';
 import { format } from 'date-fns';
+import { EntityNotFoundError } from 'typeorm';
+import { ApolloError } from 'apollo-server-express';
 
+import logger from '../../../utils/logger';
 import { User } from '../../../models/User';
 import { UserService } from '../../../services/UserService';
+import { ENTITY_NOT_FOUND } from '../errorCodes';
 
 const userQueries = {
   findUser: async(
     _: any,
     { id }: { id: number }
-  ): Promise<Omit<User, 'password'>> => {
-    const userService = Container.get<UserService>('userService');
-    let user = await userService.find(id);
-    user = Object.assign(
-      user,
-      {
-        password: undefined,
-        dateOfBirth: format(user.dateOfBirth, 'yyyy-MM-dd')
-      }
-    );
+  ): Promise<Omit<User, 'password'> | void> => {
+    try {
+      const userService = Container.get<UserService>('userService');
+      let user = await userService.find(id);
+      user = Object.assign(
+        user,
+        {
+          password: undefined,
+          dateOfBirth: format(user.dateOfBirth, 'yyyy-MM-dd')
+        }
+      );
 
-    return user;
+      return user;
+    } catch (error: any) {
+      logger.error({
+        label: 'FindUser',
+        message: error.message
+      });
+
+      let errorMessage = 'Internal server error';
+
+      if (error instanceof EntityNotFoundError) {
+        errorMessage = 'User not found'
+      }
+
+      throw new ApolloError(errorMessage, ENTITY_NOT_FOUND);
+    }
   }
 };
 
